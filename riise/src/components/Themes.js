bef181e60638da7ed0f2586a5ad9e633dfce4b13
ayugupt <ayush.gupta.10301@gmail.com>
2024-02-26 21:40:34 +0530

    wrapped theme cards to next row instead of scroll

--- riise/src/components/Themes.js
+++ riise/src/components/Themes.js
@@ -7,19 +7,18 @@ import image4 from "./Computational Gastronomy.png";
 import image5 from "./Sustainable Mobility.png";
 import image6 from "./5G-6G.png";
 import image7 from "./Advanced Multicore System.png";
-import image8 from "./healthcare_card.png"
+import image8 from "./healthcare_card.png";
 import Card from "../containers/Card";
 import RowContainer from "../containers/RowContainer";
-import { HashRouter as Router, Link } from "react-router-dom";
-import { HashRouter, Routes, Route } from 'react-router-dom';
-
+import RowOverflow from "../containers/RowOverflow";
+import { Link } from "react-router-dom";
 
 const ThemePage = () => {
   return (
     <div className="theme-container">
       <h2 className="theme-heading">THEME</h2>
       <h1 className="section-heading">BUILD FOR INDIA</h1>
-      <RowContainer scroll={false} speed={7}>
+      <RowOverflow>
         <Link to="/healthcare">
           <Card
             title={<h2>Healthcare</h2>}
@@ -29,62 +28,62 @@ const ThemePage = () => {
           />
         </Link>
         <Link to="/designforindia">
-        <Card
-          title={<h2>Design for India</h2>}
-          body="Human Centered solutions for India's unique challenges"
-          image={image1}
-          alt="India"
-        />
+          <Card
+            title={<h2>Design for India</h2>}
+            body="Human Centered solutions for India's unique challenges"
+            image={image1}
+            alt="India"
+          />
         </Link>
         <Link to="/roboticsandspace">
-        <Card 
-          title={<h2>Robotics and Space Tech</h2>}
-          body="Automating crucial tasks and unlocking space explorations"
-          image={image2}
-          alt="India"
-        />
+          <Card
+            title={<h2>Robotics and Space Tech</h2>}
+            body="Automating crucial tasks and unlocking space explorations"
+            image={image2}
+            alt="India"
+          />
         </Link>
         <Link to="/bridgingthoeryandpractice">
-        <Card
-          title={<h2>Bridging theory and practice</h2>}
-          body="Building and developing a strong foundation for innovation"
-          image={image3}
-          alt="India"
-        />
+          <Card
+            title={<h2>Bridging theory and practice</h2>}
+            body="Building and developing a strong foundation for innovation"
+            image={image3}
+            alt="India"
+          />
         </Link>
         <Link to="/computationalgastronomy">
-        <Card
-          title={<h2>Computational gastronomy</h2>}
-          body="Exploring the intersection of food, technology and culture"
-          image={image4}
-          alt="India"
-        />
+          <Card
+            title={<h2>Computational gastronomy</h2>}
+            body="Exploring the intersection of food, technology and culture"
+            image={image4}
+            alt="India"
+          />
         </Link>
         <Link to="/sustainablemobility">
-        <Card
-          title={<h2>Sustainable mobility</h2>}
-          body="Building Green, Accesible and Inclusive mobility"
-          image={image5}
-          alt="India"
-        />
+          <Card
+            title={<h2>Sustainable mobility</h2>}
+            body="Building Green, Accesible and Inclusive mobility"
+            image={image5}
+            alt="India"
+          />
         </Link>
         <Link to="/5G-6G">
-        <Card
-          title={<h2>5G/6G </h2>}
-          body="Powering India's Digital transformation on web connectivity"
-          image={image6}
-          alt="India"
-        />
+          <Card
+            title={<h2>5G/6G </h2>}
+            body="Powering India's Digital transformation on web connectivity"
+            image={image6}
+            alt="India"
+          />
         </Link>
         <Link to="/advancemulticoresystems">
-        <Card
-          title={<h2>Advanced multicore systems</h2>}
-          body="Building indigineous high-performance computing platforms"
-          image={image7}
-          alt="India"
-        />
+          <Card
+            title={<h2>Advanced multicore systems</h2>}
+            body="Building indigineous high-performance computing platforms"
+            image={image7}
+            alt="India"
+          />
         </Link>
-      </RowContainer>
+      </RowOverflow>
     </div>
   );
 };
