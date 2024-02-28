4f963f75b827c4f6f25ec4dc3ef28149905c1f89
jaskaran04 <134089873+jaskaran04@users.noreply.github.com>
2024-02-28 03:32:07 -0800

    Update About.js
    Updated about.js

--- riise/src/components/About.js
+++ riise/src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './About.css'; // Import the CSS file for the header styles
+import './About.css'; 
 
 const About = () => {
     return (
-        <div>
-            <h1>ABOUT</h1>
+        <div className="about-container">
+            <div className="about-content">
+                <h1>ABOUT US</h1>
+                <p>Research Innovation and Incubation Showcase Events (RIISE), the annual flagship event of IIIT Delhi, brings together the brightest minds from academia, industry, and government under a roof, building a platform that transcends boundaries, transforming academic brilliance into tangible impact.</p>
+            </div>
         </div>
     );
 }
