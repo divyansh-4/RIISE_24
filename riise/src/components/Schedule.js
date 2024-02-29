fa63e71c718dda9b9075a8ac299950c1ec8e0a78
sk1802 <88622678+sk1802@users.noreply.github.com>
2024-02-29 23:05:55 +0530

    schedule page update

--- riise/src/components/Schedule.js
+++ riise/src/components/Schedule.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import './Schedule.css'; // Import the CSS file for the header styles
-import sched_logo from './Sched-img.png';
+// import sched_logo from '';
 
 
 const Schedule = ({}) => {
@@ -11,8 +11,11 @@ const Schedule = ({}) => {
       <div className="event-text">EVENTS</div>
       <div className="schedule-text">SCHEDULE</div>
       <div className="date-text">15TH - 16TH MARCH</div>
-      <div className="coming-soon-text">COMING SOON</div>
-      <img src={sched_logo}  className="imge" />
+      {/* <div className="coming-soon-text">COMING SOON</div> */}
+      {/* <div>
+        <img src={"schedule.svg"}  className="image" />
+      </div> */}
+      <img src={"schedule.svg"}  className="image" />
     </div>
   );
 }
