613fae95b6c070fa722bb59ea227e5d5676d6875
ayugupt <ayush.gupta.10301@gmail.com>
2024-03-01 09:18:33 +0530

    keynote font fix

--- riise/src/components/Keynote.js
+++ riise/src/components/Keynote.js
@@ -9,7 +9,7 @@ const Keynote = () => {
       <h2 className="theme-heading2">EVENTS</h2>
       <h1 className="section-heading2">KEYNOTE SPEAKERS</h1>
       <div className="keynote-card-row-container">
-        <RowOverflow center={true} countRelWidth={[5, 5, 3, 3, 3]}>
+        <RowOverflow center={true} countRelWidth={[5, 5, 3, 3, 1]}>
           <Card
             title="Jaswinder Chadha"
             body="President & CEO @ Axtria | AI Software for Life Science Industry"
